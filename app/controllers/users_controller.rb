13c86d4dc9a5983a2cc6e772b537bbe5e3170ea3
scriptmastership <scriptmaster.team@gmail.com>
2016-02-23 15:04:00 -0500

    add the ability to set User Approvals

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
 
   def new
     @user = User.new
@@ -14,11 +15,33 @@ class UsersController < ApplicationController
     end
   end
 
+  def index
+    @users = User.where(owner_id: current_user.owner_id)
+  end
+
+  def edit
+  end
+
+  def update
+    respond_to do |format|
+      if @user.update(user_params)
+        format.html { redirect_to "/users", notice: 'User was successfully updated.' }
+        format.json { render :show, status: :ok, location: @user }
+      else
+        format.html { render :edit }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
   private
+  def set_user
+    @user = User.find(params[:id])
+  end
 
   # Never trust parameters from the scary internet, only allow the white list through.
   def user_params
-    params.require(:user).permit(:email, :owner_id, :password, :password_confirmation)
+    params.require(:user).permit(:email, :owner_id, :password, :password_confirmation, :approved)
   end
 
 
