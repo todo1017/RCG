bea824b174da6e0b4dc8953fe324631ab601854f
scriptmastership <scriptmaster.team@gmail.com>
2016-02-28 21:48:11 -0500

    format $ fields

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,29 @@ module ApplicationHelper
     if field == nil || field == "" then " class=yellow-td" end
   end
 
+  def dollar_monthly(field)
+    if field != nil && field != ""
+      return "$" + field.round.to_s + "/mo"
+    end
+    return ""
+  end
+  def dollar_yearly(field)
+    if field != nil && field != ""
+      return "$" + field.round.to_s + "/yr"
+    end
+    return ""
+  end
+  def dollar_onetime(field)
+    if field != nil && field != ""
+      return "$" + field.round.to_s + " one-time"
+    end
+    return ""
+  end
+  def number_weeks(field)
+    if field != nil && field != ""
+      return field.to_s + " months"
+    end
+    return ""
+  end
+
 end
