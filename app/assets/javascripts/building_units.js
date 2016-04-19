0f9f215012b104eb228d6d2721678cb8b52fd14e
scriptmastership <scriptmaster.team@gmail.com>
2016-04-18 20:37:27 -0400

    Place excel download links properly; add sorting to comp index table

--- app/assets/javascripts/building_units.js
+++ app/assets/javascripts/building_units.js
@@ -1,18 +1,28 @@
 (function(){
-	var init = function() {
-		if (!document.getElementById('js-save-and-add')) {
-			return;
-		};
+  var init = function() {
 
-		document.getElementById('js-save-and-add').addEventListener('click', function(e) {
-			e.preventDefault();
-			var $form = $(e.currentTarget).closest('form');
-			var $checkbox = $form.find('#building_unit_move_in');
-			var $submitButton = $form.find('input[type="submit"]');
-			$checkbox[0].checked = 'checked';
-			$submitButton.click();
-		});
-	}
+    if (document.getElementById('js-save-and-add')) {
+      document.getElementById('js-save-and-add').addEventListener('click', function(e) {
+        e.preventDefault();
+        var $form = $(e.currentTarget).closest('form');
+        var $checkbox = $form.find('#building_unit_move_in');
+        var $submitButton = $form.find('input[type="submit"]');
+        $checkbox[0].checked = 'checked';
+        $submitButton.click();
+      });
+    }
 
-	window.addEventListener('load', init);
+    if (document.getElementById('comp-index-table')) {
+      $('#comp-index-table').dataTable({
+        "aoColumnDefs": [
+          { "bSortable": false, "aTargets": [ 1, 2, 3, 4, 5, 6, 7, 8, 11 ] }
+        ],
+        bPaginate: false,
+        bInfo: false,
+        bFilter: false
+      });
+    }
+  }
+
+  window.addEventListener('load', init);
 })()
