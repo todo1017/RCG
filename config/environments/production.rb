533e302670a7ebd66847ad8fa32c50c138d1c448
scriptmastership <scriptmaster.team@gmail.com>
2016-07-21 19:43:15 -0400

    configure sendgrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,13 +82,13 @@ Rails.application.configure do
   # Change mail delvery to either :smtp, :sendmail, :file, :test
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-      address: "smtp.gmail.com",
-      port: 587,
-      domain: "morning-beyond-60071.herokuapp.com",
-      authentication: "plain",
-      enable_starttls_auto: true,
-      user_name: ENV["GMAIL_USERNAME"],
-      password: ENV["GMAIL_PASSWORD"]
+      :address        => 'smtp.sendgrid.net',
+      :port           => '587',
+      :authentication => :plain,
+      :user_name      => ENV['SENDGRID_USERNAME'],
+      :password       => ENV['SENDGRID_PASSWORD'],
+      :domain         => 'heroku.com',
+      :enable_starttls_auto => true
   }
 
 
