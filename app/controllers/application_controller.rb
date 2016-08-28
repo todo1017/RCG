dcd454afbe82ee38fcc67c4645e75a088d6bdb1e
scriptmastership <scriptmaster.team@gmail.com>
2016-08-28 14:42:41 -0400

    fix some login bugs

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  before_filter :require_login
 
   private
 
@@ -13,6 +14,12 @@ class ApplicationController < ActionController::Base
 
   def authorize
     redirect_to login_url, alert: "Not authorized" if current_user.nil?
+  end
+
+  def require_login
+    unless current_user
+      redirect_to login_url
+    end
   end
 
 end
