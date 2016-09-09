fb655bcc1f10e048b6ad8efbc618618211fbb3fc
scriptmastership <scriptmaster.team@gmail.com>
2016-09-09 17:14:59 -0400

    persist and pre-populate user_buildings

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -47,17 +47,23 @@ class UsersController < ApplicationController
           owner_user.save
           render partial: "/users/geographies", locals: { dpm_admin: owner_user.dpm_admin, owner_id: owner_user.owner_id, user_id: owner_user.user_id }
         elsif params[:geography_id].present?
+          UserGeography.destroy_all(user_id: params[:user_id], geography_id: params[:geography_id])
           if params[:selection] == "none"
-            UserGeography.destroy_all(user_id: params[:user_id], geography_id: params[:geography_id])
-            render partial: "/users/buildings", locals: { user_id: params[:user_id], geography_id: params[:geography_id], selection: "none" }
+            render partial: "/users/buildings", locals: { user_id: params[:user_id], geography_id: params[:geography_id], access_type: "none" }
           elsif params[:selection] == "partial"
-            UserGeography.destroy_all(user_id: params[:user_id], geography_id: params[:geography_id])
-            render partial: "/users/buildings", locals: { user_id: params[:user_id], geography_id: params[:geography_id], selection: "partial" }
+            UserGeography.find_or_create_by(user_id: params[:user_id], geography_id: params[:geography_id], access_type: "partial")
+            render partial: "/users/buildings", locals: { user_id: params[:user_id], geography_id: params[:geography_id], access_type: "partial" }
           elsif params[:selection] == "all"
-            UserGeography.find_or_create_by(user_id: params[:user_id], geography_id: params[:geography_id])
-            render partial: "/users/buildings", locals: { user_id: params[:user_id], geography_id: params[:geography_id], selection: "all" }
+            UserGeography.find_or_create_by(user_id: params[:user_id], geography_id: params[:geography_id], access_type: "all")
+            render partial: "/users/buildings", locals: { user_id: params[:user_id], geography_id: params[:geography_id], access_type: "all" }
           end
         elsif params[:building_id].present?
+          user_buildings = UserBuilding.find_by(user_id: params[:user_id], building_id: params[:building_id])
+          if user_buildings == nil
+            UserBuilding.create(user_id: params[:user_id], building_id: params[:building_id])
+          else
+            user_buildings.destroy
+          end
         end
       }
     end
